749ddb9b0d0d77cb6172f9bc1531b0597161466c
pierre <pierrelambert41@gmail.com>
2017-09-05 14:40:14 +0200

    total revue de mon code

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
+
 $students = [
-    "Emmanuel" => 42,
-    "Thierry" => 51,
-    "Pascal" => 45,
-    "Eric" => 48,
-    "Nicolas" => 19
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
 ];
+
+$totalage = 0;
+$nbeleve = count($students);
+
+foreach ($students as $key => $value){
+
+    echo $key." ".$students[$key]." ans <br/>";
+    $totalage += $students[$key];
+
+}
+
+echo "la moyenne d'age est de ".($totalage / $nbeleve)." années";
